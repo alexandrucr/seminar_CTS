6e1ef179446a1692ff0422050908d4d6cc4de80f
alexandrucr <63010947+alexandrucr@users.noreply.github.com>
2021-02-25 15:20:59 +0200

    Add files via upload
    modif+nemodif

--- CringureanuIonutAlexandru_1054_modificari.cpp
+++ CringureanuIonutAlexandru_1054_modificari.cpp
@@ -8,6 +8,15 @@ Tabacherie, Trabuc, Bricheta
 
 Observatii:
 [-std=c++11] language standard checked
+
+MODIFICARI:
+- pastrare conventie de denumire a metodelor
+ex: setnrBrichete > setNrBrichete, masjuscule pt fiecare cuvant
+- scrierea a unor noi functii pentru a nu repeta cod in main
+- redenumirea variabilelor mai sugestiva sau comentarea acestora pentru a arata ca sunt folosite pentru o portiune mica in main
+- stergerea unor comentarii redundante precum
+"/// (string numeP, int varstaP, float salariuP, int vechimeP, float comisionP, string sucursalaP)"
+- delimitari mai clare in ceea ce priveste cerinta proiectului
 */
 #include <iostream>
 #include <string>
